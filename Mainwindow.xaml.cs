e3bbbfd149a20af7ddfa859724f8c62543e66294
Aidan McClung <36234902+MysticalWarrior@users.noreply.github.com>
2018-04-18 15:08:35 -0400

    Update Mainwindow.xaml.cs

--- Mainwindow.xaml.cs
+++ Mainwindow.xaml.cs
@@ -23,10 +23,37 @@ namespace u3AidanMHangman
     /// </summary>
     public partial class MainWindow : Window
     {
+        string[] goals = new string[9];
+        string[] guessed = new string[9];
+        //StreamReader streamreader = new StreamReader("https://raw.githubusercontent.com/MysticalWarrior/u3AidanMHangman/master/list.txt");
 
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnSetWord_Click(object sender, RoutedEventArgs e)
+        {
+            string tempWord = "lineage"; //streamreader.ReadLine();
+            MessageBox.Show(tempWord);
+            //add to array
+            for (int i = 0; i < tempWord.Length; i++)
+            {
+                goals[i] = tempWord.Substring(i, 1);
+                //MessageBox.Show(goals[i]);
+            }
+            //add underlines
+            lblUnderlines.Content = "";
+            for (int i = 0; i < tempWord.Length; i++)
+            {
+                lblUnderlines.Content += "_ ";
+            }
+            MessageBox.Show("word set");
+        }
+
+        private void btnGuessLetter_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
